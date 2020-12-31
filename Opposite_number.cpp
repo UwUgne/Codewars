be1da92dad1e4988f97712e9524fb48a78f7f733
Ugnė Klimavičiūtė <76807334+UwUgne@users.noreply.github.com>
2020-12-31 10:58:24 +0200

    Add files via upload

---
+++ Opposite_number.cpp
@@ -0,0 +1,6 @@
+#include "Opposite_number.h"
+class Opposite_number
+	int opposite(int number)
+{
+
+	return -number;
---
+++ multiply.cpp
@@ -0,0 +1,6 @@
+#include "multiply.h"
+class multiply
+	int multiply(int a, int b)
+{
+	return a * b;
+}
